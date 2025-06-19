43ef47f9d715aecca31092f3cefec95d8a2c6509
Sabbir Rayhan Mahee <sabbirrayhan475@gmail.com>
2025-06-19 15:40:20 +0600

    conole log

--- two_sum.js
+++ two_sum.js
@@ -2,17 +2,17 @@
 
 let nums = [2,7,11,15], target = 9;
 
-
-
-var twoSum = function(nums, target) {
-    let map  = new Map();
+var twoSum = function(nums,target){
+    let map = new Map();
     for(let i=0; i<nums.length; i++){
         if(map.has(target-nums[i])){
-            return [i,map.get(target-nums[i])]
+            return [map.get(target-nums[i]),i]
         }else{
-            map.set(nums[i],i)
+            map.set(nums[i],i);
         }
     }
 
     return [-1,-1]
-};
+}
+
+console.log(twoSum(nums,target))
